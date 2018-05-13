cc940d4ec6bf912c59e0e38f9a42363515c11b63
Otto Nordgren <otto.nordgren@gmail.com>
2018-05-13 22:10:00 +0200

    move admin

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 
+gulp.task('build', ['admin', 'pug', 'sass']);
 
-gulp.task('build', ['pug', 'sass']);
-
+gulp.task('admin', () => {
+  return gulp.src('src/admin/*')
+    .pipe(gulp.dest('./public/admin'));
+});
 
 gulp.task('pug', () => {
   return gulp.src('src/**/*.pug')
