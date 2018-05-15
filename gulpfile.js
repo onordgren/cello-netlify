b5f7caad292d42f6a68074304a02e7dbeaf0ee8e
Otto Nordgren <otto.nordgren@gmail.com>
2018-05-15 17:52:37 +0200

    gulp gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp');
-const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const pug = require('pug');
+const map = require('map-stream');
+const rename = require('gulp-rename');
+const glob = require('glob');
+const fs = require('fs');
+const file = require('gulp-file');
+const path = require('path');
 
-gulp.task('build', ['admin', 'pug', 'sass']);
+gulp.task('build', ['admin', 'sass', 'images', 'categories']);
 
 gulp.task('admin', () => {
-  return gulp.src('src/admin/*')
+  return gulp.src('static/admin/*')
     .pipe(gulp.dest('./public/admin'));
 });
 
@@ -17,9 +23,41 @@ gulp.task('pug', () => {
     .pipe(gulp.dest('./public'));
 });
 
-
 gulp.task('sass', () => {
-  return gulp.src('src/**/*.scss')
+  return gulp.src('static/css/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public/css'));
 });
+
+gulp.task('images', () => {
+  return gulp.src('static/images/**')
+    .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('categories', () => {
+    const categoryTemplate = pug.compileFile('layouts/category.pug');
+    const productTemplate = pug.compileFile('layouts/product.pug');
+    const products = glob.sync('_products/*.json').map(getJsonFromFile);
+    const categories = glob.sync('_categories/*.json').map(getJsonFromFile);
+
+    categories.forEach((category) => {
+      const categoryDir = `./public/${category.name}`;
+      const productsInCategory = products.filter(product => product.category === category.title);
+      productsInCategory.map(product => product.permaLink = `/${category.name}/${product.name}.html`);
+      category.products = productsInCategory;
+
+      file('index.html', categoryTemplate(category), { src: true })
+        .pipe(gulp.dest(categoryDir));
+
+      productsInCategory.forEach((product) => {
+        file(`${product.name}.html`, productTemplate(product), { src: true })
+          .pipe(gulp.dest(categoryDir));
+      })
+    });
+});
+
+function getJsonFromFile(file) {
+  let json = JSON.parse(fs.readFileSync(file));
+  json.name = path.parse(file).name;
+  return json;
+}
